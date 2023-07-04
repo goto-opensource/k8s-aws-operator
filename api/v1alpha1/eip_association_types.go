4ffd565aa5f834b59de5f80aca9db9a492eecac8
Antoine Labonté <99213939+alabonte7@users.noreply.github.com>
2023-07-04 09:57:47 -0400

    feat: adding EIPAssociation CRD and controller to allow static EIP unassignment (#16)

---
+++ api/v1alpha1/eip_association_types.go
@@ -0,0 +1,46 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type EIPAssociationSpec struct {
+	Assignment *EIPAssignment `json:"assignment,omitempty"`
+	EIPName    string         `json:"eipName,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:printcolumn:name="Pod Name",type=string,JSONPath=`.spec.assignment.podName`
+// +kubebuilder:printcolumn:name="EIP Name",type=string,JSONPath=`.spec.eipName`
+type EIPAssociation struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec EIPAssociationSpec `json:"spec,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type EIPAssociationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EIPAssociation `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EIPAssociation{}, &EIPAssociationList{})
+}
---
+++ controllers/eip_association_controller.go
@@ -0,0 +1,98 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	awsv1alpha1 "github.com/logmein/k8s-aws-operator/api/v1alpha1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// EIPReconciler reconciles a EIP object
+type EIPAssociationReconciler struct {
+	client.Client
+	Log logr.Logger
+}
+
+// +kubebuilder:rbac:groups=aws.k8s.logmein.com,resources=eipassociations,verbs=get;list;watch;create;update;patch;delete
+
+func (r *EIPAssociationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := r.Log.WithValues("eipAssociation", req.NamespacedName)
+
+	var eipAssociation awsv1alpha1.EIPAssociation
+	if err := r.Get(ctx, req.NamespacedName, &eipAssociation); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if eipAssociation.ObjectMeta.DeletionTimestamp.IsZero() {
+		if !containsString(eipAssociation.ObjectMeta.Finalizers, finalizerName) {
+			eipAssociation.ObjectMeta.Finalizers = append(eipAssociation.ObjectMeta.Finalizers, finalizerName)
+			log.Info("New EIP Association")
+			var eip awsv1alpha1.EIP
+			if err := r.Client.Get(ctx, client.ObjectKey{
+				Namespace: req.Namespace,
+				Name:      eipAssociation.Spec.EIPName,
+			}, &eip); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			if eip.Spec.Assignment == nil && eip.Status.State == "allocated" {
+				eip.Spec.Assignment = eipAssociation.Spec.Assignment
+			} else {
+				return ctrl.Result{}, fmt.Errorf("Cannot assign EIP because it isn't in allocated state.")
+			}
+
+			if err := r.Update(ctx, &eip); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			return ctrl.Result{}, r.Update(ctx, &eipAssociation)
+		}
+	} else {
+		// Association is being deleted we want to unassign EIP
+		if containsString(eipAssociation.ObjectMeta.Finalizers, finalizerName) {
+			var eip awsv1alpha1.EIP
+			if err := r.Client.Get(ctx, client.ObjectKey{
+				Namespace: req.Namespace,
+				Name:      eipAssociation.Spec.EIPName,
+			}, &eip); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			if eip.Status.Assignment != nil && (eip.Status.Assignment.PodName == eipAssociation.Spec.Assignment.PodName || eip.Status.Assignment.ENI == eipAssociation.Spec.Assignment.ENI) {
+				log.Info("Unassigning corresponding EIP")
+				eip.Spec.Assignment = nil
+				if err := r.Update(ctx, &eip); err != nil {
+					return ctrl.Result{}, err
+				}
+			}
+			eipAssociation.ObjectMeta.Finalizers = removeString(eipAssociation.ObjectMeta.Finalizers, finalizerName)
+			return ctrl.Result{}, r.Update(ctx, &eipAssociation)
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *EIPAssociationReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&awsv1alpha1.EIPAssociation{}).
+		Complete(r)
+}
